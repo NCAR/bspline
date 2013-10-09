fa1ecd84d7ee5bea7696c5aaf5aa8f524116dedc
martinc <martinc@da89c675-a4d7-0310-8a15-9e7546503515>
2013-10-09 20:33:51 +0000

    Tool definitions created for AspenQC, and AspenQC builds.
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/bspline/trunk@8792 da89c675-a4d7-0310-8a15-9e7546503515

---
+++ BSpline/tool_bspline.py
@@ -0,0 +1,40 @@
+# -*- python -*-
+
+import eol_scons
+
+# The tool name and the library name will be the same.
+toolname = 'bspline'
+
+thisDir = Dir('.').abspath
+upDir   = Dir('./../').abspath
+
+# Define tool. This must match the value of toolname.
+def bspline(env):
+    env.Append(LIBS = [toolname])
+    env.AppendUnique(CPPPATH = [upDir])
+    env.AppendUnique(CPPPATH = [thisDir])
+    env.AppendUnique(LIBPATH = [thisDir])
+    ourtools = ['doxygen', 'prefixoptions']
+    env.Require(ourtools)
+    
+Export(toolname)
+
+# Build library
+sources = Split("""
+BSplineLib.cpp
+""")
+
+headers = Split("""
+BSplineBase.h
+BSpline.h
+""")
+
+env = Environment(tools = ['default', toolname])
+
+lib = env.Library(toolname, sources)
+env.Default(lib)
+
+# Create doxygen
+doxref = env.Apidocs(sources + headers, DOXYFILE_DICT={'PROJECT_NAME':toolname, 'PROJECT_NUMBER':'1.0'})
+env.Default(doxref)
+
