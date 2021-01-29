c17d0d6d6b41607373e40c26d048fa54fe9728a9
Gary Granger <granger@ucar.edu>
2021-01-29 07:15:10 -0700

    consolidate build into tool_bspline
    Rather than duplicate build configuration between SConscript and
    tool_bspline.py, move all building into the tool.  Add the static
    library directly to LIBS instead of modifying LIBPATH.  The top-level
    SConstruct calls the tool directly with SConscript(), and also adds
    the testing targets.

--- BSpline/tool_bspline.py
+++ BSpline/tool_bspline.py
@@ -1,34 +1,80 @@
-toolname = 'bspline'
 
-thisDir = Dir('.').abspath
-upDir   = Dir('./../').abspath
+from SCons.Script import Environment, Copy, Export
 
-# Define tool. This must match the value of toolname.
+env = Environment(tools=['default', 'doxygen', 'gitinfo'])
+
+# Since this is often a submodule, make sure to use the git version info for
+# this subdirectory.
+env.LoadGitInfo('.')
+version = env.get('REPO_REVISION', 'unknown')
+
+# If git revision info is loaded successfully, then set the auto-revision,
+# otherwise the default will be used.  Someday the default version could be
+# extracted from BSplineVersion.h so it can be passed to doxygen.
+if version != 'unknown':
+    env.Append(CPPDEFINES=['BSPLINE_AUTO_REVISION="%s"' % (version)])
+
+sources = ['BSplineLib.cpp']
+
+bsplinelib = env.Library('bspline', sources)
+
+env.Default(bsplinelib)
+
+tooldir = env.Dir('.')
+
+
 def bspline(env):
-    env.Append(LIBS = [toolname])
-    env.AppendUnique(CPPPATH = [upDir])
-    env.AppendUnique(CPPPATH = [thisDir])
-    env.AppendUnique(LIBPATH = [thisDir])
-    env.Require(['prefixoptions'])
-    
-Export(toolname)
+    # The library is static, so add it directly to LIBS.
+    env.Append(LIBS=[bsplinelib])
+    # Includes are qualified with the BSpline directory, so add the parent
+    # to CPPPATH.
+    env.AppendUnique(CPPPATH=[tooldir.Dir('..')])
 
-# Build library
-sources = Split("""
-BSplineLib.cpp
+
+Export('bspline')
+
+
+# Setup apidocs
+
+IMAGES = env.Split("""
+plot-1.png plot-2.png plot-3.png plot-4.png plot-5.png plot-6.png
 """)
 
-headers = Split("""
-BSplineBase.h
-BSpline.h
+IMAGES = [env.File("../Tests/R/" + f) for f in IMAGES]
+
+docfiles = env.Split("""
+ ../README.md
+ BSpline.dox
+ BSplineLib.cpp
+ BSpline.cpp
+ BSpline.h
+ BSplineBase.cpp
+ BSplineBase.h
+ BandedMatrix.h
 """)
+docfiles.extend(IMAGES)
 
-env = Environment(tools = ['default', 'doxygen', toolname])
+doxyfile_text = """
+EXTRACT_ALL            = NO
+EXTRACT_STATIC         = NO
+EXTRACT_PRIVATE        = NO
 
-lib = env.Library(toolname, sources)
-env.Default(lib)
+SOURCE_BROWSER         = NO
+REFERENCED_BY_RELATION = NO
+CLASS_GRAPH            = YES
+COLLABORATION_GRAPH    = NO
+INCLUDE_GRAPH          = NO
+INCLUDED_BY_GRAPH      = NO
+ALPHABETICAL_INDEX     = NO
 
-# Create doxygen
-doxref = env.Apidocs(sources + headers + ['BSpline.dox'],
-                     DOXYFILE_DICT={'PROJECT_NAME':toolname,
-                                    'PROJECT_NUMBER':'1.0'})
+GRAPHICAL_HIERARCHY    = YES
+"""
+
+docs = env.Apidocs(docfiles,
+                   DOXYFILE_TEXT=doxyfile_text,
+                   DOXYFILE_DICT={'PROJECT_NAME': 'EOL BSpline Library',
+                                  'PROJECT_NUMBER': version})
+
+env.AddPostAction(docs, [Copy(docs[0].dir, f) for f in IMAGES])
+
+env.Alias('doc', docs)
--- SConstruct
+++ SConstruct
@@ -1,55 +1,12 @@
 # -*- python -*-
 
 import eol_scons
-from SCons.Script import Environment, Copy
+from SCons.Script import Environment
 
-env = Environment(tools=['default', 'doxygen'])
+env = Environment(tools=['default'])
 
-env.Export('env')
+# The primary builds are in the tool.  This file adds the testing products,
+# for when the bspline is source is built by itself.
 
-env.SConscript('SConscript')
+env.SConscript('BSpline/tool_bspline.py')
 env.SConscript('Tests/C++/SConscript')
-
-IMAGES = env.Split("""
-plot-1.png plot-2.png plot-3.png plot-4.png plot-5.png plot-6.png
-""")
-
-IMAGES = [env.File("#/Tests/R/" + f) for f in IMAGES]
-
-docfiles = env.Split("""
- README.md
- BSpline/BSpline.dox
- BSpline/BSplineLib.cpp
- BSpline/BSpline.cpp
- BSpline/BSpline.h
- BSpline/BSplineBase.cpp
- BSpline/BSplineBase.h
- BSpline/BandedMatrix.h
-""")
-docfiles.extend([str(f) for f in IMAGES])
-
-doxyfile_text = """
-EXTRACT_ALL	       = NO
-EXTRACT_STATIC	       = NO
-EXTRACT_PRIVATE	       = NO
-
-SOURCE_BROWSER         = NO
-REFERENCED_BY_RELATION = NO
-CLASS_GRAPH            = YES
-COLLABORATION_GRAPH    = NO
-INCLUDE_GRAPH          = NO
-INCLUDED_BY_GRAPH      = NO
-ALPHABETICAL_INDEX     = NO
-
-GRAPHICAL_HIERARCHY    = YES
-"""
-
-version = "%(REPO_REVISION)s" % env
-docs = env.Apidocs(docfiles,
-                   DOXYFILE_TEXT=doxyfile_text,
-                   DOXYFILE_DICT={'PROJECT_NAME': 'EOL BSpline Library',
-                                  'PROJECT_NUMBER': version})
-
-env.AddPostAction(docs, [Copy(docs[0].dir, f) for f in IMAGES])
-
-env.Alias('doc', docs)
