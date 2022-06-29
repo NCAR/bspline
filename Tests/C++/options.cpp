4f55b24d395173c074bad419a927bcdf713ce27c
Gary Granger <granger@ucar.edu>
2022-06-29 17:18:17 -0600

    not our code, so we shouldn't copyright it
    I think it's public domain.

--- Tests/C++/options.cpp
+++ Tests/C++/options.cpp
@@ -1,18 +1,3 @@
-/************************************************************************
- * Copyright 2009 University Corporation for Atmospheric Research.
- * All rights reserved.
- *
- * Use of this code is subject to the standard BSD license:
- *
- *  http://www.opensource.org/licenses/bsd-license.html
- *
- * See the COPYRIGHT file in the source distribution for the license text,
- * or see this web page:
- *
- *  http://www.eol.ucar.edu/homes/granger/bspline/doc/
- *
- *************************************************************************/
-
 // ****************************************************************************
 // ^FILE: options.c - implement the functions defined in <options.h>
 //
